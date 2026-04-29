24dd3ad2469c0be8e1ea60a5e125b4f8a9395b0d
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2026-04-29 09:42:32 -0300

    Add FlowingCode/BadgeList default branch

--- EcosystemBuild.java
+++ EcosystemBuild.java
@@ -99,7 +99,7 @@ public class EcosystemBuild implements Callable<Integer> {
             name = "Badge List Add-on";
             repoUrl = "https://github.com/FlowingCode/BadgeList";
             notifyUsers = List.of("javier-godoy", "scardanzan", "paodb");
-            versionOverrides = Map.of("24.*", new VersionConfig() {{ branch = "1.x"; }}, "25.*", new VersionConfig() {{ branch = "1.x"; profile = "v25"; }});
+            versionOverrides = Map.of("24.*", new VersionConfig() {{ branch = "1.x"; }}, "25.*", new VersionConfig() {{ branch = "1.x"; profile = "v25"; }}, "25.1+", new VersionConfig());
         }},
         new AddonProject() {{
             name = "Carousel Addon";
